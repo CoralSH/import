c00d755c90d6830a77d49dd222ca1fbfa097e92d
Nimit Kalra <nimit@nimitkalra.com>
2016-01-03 00:09:06 -0600

    Initial commit.

---
+++ package.sh
@@ -0,0 +1,7 @@
+name="import"
+version="0.0.1"
+description="Modules in Shell!"
+author="Nimit Kalra <me@nimit.io> (http://nimit.io)"
+main="lib/import.sh"
+keywords=("coral-core", "modules", "import")
+license="MIT"
